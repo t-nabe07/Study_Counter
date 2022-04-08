52e82355b37b17b883d6bd9824ff77a203ad703a
t-nabe07 <watanabe.t.job@gmail.com>
2022-04-08 08:37:13 +0000

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20220407122747) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string   "name"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true
